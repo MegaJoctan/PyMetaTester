4307f55a7e1bb1547cf4587f99600b836a154bc3
Omega Joctan <omegajoctan@gmail.com>
2025-12-26 09:46:11 +0300

    ticks and rates test functions

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from Trade.Trade import CTrade
 from datetime import datetime, timedelta
 import time
 import pytz
-from trade_simulator import TradeSimulator
+from simulator import Simulator
 
 
 if not mt5.initialize(): # Initialize MetaTrader5 instance
@@ -13,15 +13,42 @@ if not mt5.initialize(): # Initialize MetaTrader5 instance
     quit()
 
 
-sim = TradeSimulator(simulator_name="MySimulator", mt5_instance=mt5, deposit=1078.30, leverage="1:500")
+sim = Simulator(simulator_name="MySimulator", mt5_instance=mt5, deposit=1078.30, leverage="1:500")
 
+start = datetime(2025, 1, 1)
+end = datetime(2025, 1, 5)
+
+bars = 10
+symbol = "EURUSD"
+timeframe = mt5.TIMEFRAME_H1
+
+sim.Start(IS_TESTER=True)
+# rates = sim.copy_rates_from(symbol=symbol, timeframe=mt5.TIMEFRAME_H1, date_from=start, count=bars)
+# rates = sim.copy_rates_from_pos(symbol=symbol, timeframe=timeframe, start_pos=0, count=bars)
+# rates = sim.copy_rates_range(symbol=symbol, timeframe=timeframe, date_from=start, date_to=end)
+# print("is_tester=true\n", rates)
+
+ticks = sim.copy_ticks_from(symbol=symbol, date_from=start.replace(month=12, hour=0, minute=0), count=bars)
+print("is_tester=true\n", ticks)
+
+sim.Start(IS_TESTER=False) # start the simulator in real-time trading
+
+# rates = sim.copy_rates_from_pos(symbol=symbol, timeframe=timeframe, start_pos=0, count=bars)
+# rates = sim.copy_rates_from(symbol=symbol, timeframe=timeframe, date_from=start, count=bars)
+# rates = sim.copy_rates_range(symbol=symbol, timeframe=timeframe, date_from=start, date_to=end)
+# print("is_tester=false\n",rates)
+
+ticks = sim.copy_ticks_from(symbol=symbol, date_from=start.replace(month=12, hour=0, minute=0), count=bars)
+print("is_tester=false\n", ticks)
+
+"""    
 magic_number = 123456
 slippage = 10
 
 sim.set_magicnumber(magic_number=magic_number) #sets the magic number of a simulator
 sim.set_deviation_in_points(deviation_points=slippage) # sets slippage of the simulator
 
-symbol = "EURUSD"
+symbol = symbol
 m_symbol = CSymbolInfo(mt5_instance=mt5)
 m_symbol.name(symbol_name=symbol) # sets the symbol name for the class CSymbolInfo
 
@@ -62,7 +89,6 @@ while True:
     
     time.sleep(1) # sleep for one second    
 
-"""    
 # Market Orders
 
 sim.buy(volume=0.1, symbol=symbol, open_price=m_symbol.ask())
