c48c016bb7de0724df44a9ec10c3d5c8d46c73c6
Omega Joctan <omegajoctan@gmail.com>
2026-01-06 09:28:37 +0300

    test update

--- test.py
+++ test.py
@@ -1,140 +1,153 @@
 import MetaTrader5 as mt5
-from Trade.SymbolInfo import CSymbolInfo
 from Trade.Trade import CTrade
 from datetime import datetime, timedelta
 import time
 import pytz
-from simulator import Simulator
-
+from simulator import Simulator, CTrade
 
 if not mt5.initialize(): # Initialize MetaTrader5 instance
     print(f"Failed to Initialize MetaTrader5. Error = {mt5.last_error()}")
     mt5.shutdown()
     quit()
 
-
 sim = Simulator(simulator_name="MySimulator", mt5_instance=mt5, deposit=1078.30, leverage="1:500")
 
-start = datetime(2025, 1, 1)
-end = datetime(2025, 1, 5)
-
-bars = 10
 symbol = "EURUSD"
 timeframe = mt5.TIMEFRAME_H1
 
-sim.Start(IS_TESTER=True)
-# rates = sim.copy_rates_from(symbol=symbol, timeframe=mt5.TIMEFRAME_H1, date_from=start, count=bars)
-# rates = sim.copy_rates_from_pos(symbol=symbol, timeframe=timeframe, start_pos=0, count=bars)
-# rates = sim.copy_rates_range(symbol=symbol, timeframe=timeframe, date_from=start, date_to=end)
-# print("is_tester=true\n", rates)
+m_trade = CTrade(simulator=sim, magic_number=112233, filling_type_symbol=symbol, deviation_points=100)
 
-ticks = sim.copy_ticks_from(symbol=symbol, date_from=start.replace(month=12, hour=0, minute=0), count=bars)
-print("is_tester=true\n", ticks)
 
+mt5_ticks = mt5.symbol_info_tick(symbol) # tick source
+sim.TickUpdate(symbol=symbol, tick=mt5_ticks) # very important
+
+tick_from_sim = sim.symbol_info_tick(symbol=symbol) # we get ticks back from a class
+
+ask = tick_from_sim.ask
+bid = tick_from_sim.bid
+
+symbol_info = sim.symbol_info(symbol=symbol)
+lotsize = symbol_info.volume_min
+
+"""
+m_trade.buy(
+    volume=lotsize,
+    symbol=symbol,
+    price=ask,
+    sl=ask - 100 * symbol_info.point,
+    tp=ask + 150 * symbol_info.point,
+    comment="Market Buy"
+)
+
+m_trade.sell(
+    volume=lotsize,
+    symbol=symbol,
+    price=bid,
+    sl=bid + 100 * symbol_info.point,
+    tp=bid - 150 * symbol_info.point,
+    comment="Market Sell"
+)
+
+buy_limit_price = ask - 200 * symbol_info.point
+
+m_trade.buy_limit(
+    volume=lotsize,
+    symbol=symbol,
+    price=buy_limit_price,
+    sl=buy_limit_price - 100 * symbol_info.point,
+    tp=buy_limit_price + 200 * symbol_info.point,
+    comment="Buy Limit"
+)
+
+sell_limit_price = bid + 200 * symbol_info.point
+
+m_trade.sell_limit(
+    volume=lotsize,
+    symbol=symbol,
+    price=sell_limit_price,
+    sl=sell_limit_price + 100 * symbol_info.point,
+    tp=sell_limit_price - 200 * symbol_info.point,
+    comment="Sell Limit"
+)
+
+buy_stop_price = ask + 150 * symbol_info.point
+
+m_trade.buy_stop(
+    volume=lotsize,
+    symbol=symbol,
+    price=buy_stop_price,
+    sl=buy_stop_price - 100 * symbol_info.point,
+    tp=buy_stop_price + 300 * symbol_info.point,
+    comment="Buy Stop"
+)
+
+sell_stop_price = bid - 150 * symbol_info.point
+
+m_trade.sell_stop(
+    volume=lotsize,
+    symbol=symbol,
+    price=sell_stop_price,
+    sl=sell_stop_price + 100 * symbol_info.point,
+    tp=sell_stop_price - 300 * symbol_info.point,
+    comment="Sell Stop"
+)
+"""
+
+
+# print(f"positions in a simulator = {sim.positions_total()}:\n",sim.positions_get())
+# print(f"orders in a simulator = {sim.orders_total()}:\n", sim.orders_get())
+"""
+
+m_trade.buy(volume=lotsize, symbol=symbol, price=ask, comment="buy pos")
+m_trade.sell(volume=lotsize, symbol=symbol, price=bid, comment="sell pos")
+
+print(f"positions in a simulator = {sim.positions_total()}:\n",sim.positions_get())
+
+positions = sim.positions_get()
+for pos in positions:
+    # if pos.symbol == symbol and pos.type == sim.mt5_instance.POSITION_TYPE_BUY: # close a buy position
+    #     m_trade.position_close(ticket=pos.ticket, deviation=10) 
+        
+    if pos.sl == 0:
+        if pos.type == sim.mt5_instance.POSITION_TYPE_BUY:
+            m_trade.position_modify(ticket=pos.ticket, sl=pos.price_open - 100 * symbol_info.point, tp=pos.tp) 
+        if pos.type == sim.mt5_instance.POSITION_TYPE_SELL:
+            m_trade.position_modify(ticket=pos.ticket, sl=pos.price_open + 100 * symbol_info.point, tp=pos.tp) 
+        
+print("positions after modification\n: ", sim.positions_get())
+"""
+
+m_trade.buy_stop(symbol=symbol, volume=symbol_info.volume_min, price=ask+500*symbol_info.point)
+
+for order in sim.orders_get():
+    
+    print("order curr price: ", order.price_open)
+    
+    m_trade.order_modify(ticket=order.ticket, price=order.price_open+10*symbol_info.point, sl=order.sl, tp=order.tp)
+    
+    print("order moved 10 points upward", order.price_open)
+    if m_trade.order_delete(ticket=order.ticket) is None:
+        continue
+    
+    print("orders remaining: ", sim.orders_total())
+
+# for pos in sim.positions_get():
+#     m_trade.position_close(ticket=pos.ticket)
+
+# print("After close positions total in simulator: ", sim.positions_total())
+
+# now = datetime.fromtimestamp(mt5_ticks.time)
+# print("deals:\n",sim.history_deals_get(date_from=now-timedelta(days=1), date_to=now))
+
+"""
 sim.Start(IS_TESTER=False) # start the simulator in real-time trading
 
-# rates = sim.copy_rates_from_pos(symbol=symbol, timeframe=timeframe, start_pos=0, count=bars)
-# rates = sim.copy_rates_from(symbol=symbol, timeframe=timeframe, date_from=start, count=bars)
-# rates = sim.copy_rates_range(symbol=symbol, timeframe=timeframe, date_from=start, date_to=end)
-# print("is_tester=false\n",rates)
+m_trade.buy(volume=0.01, symbol=symbol, price=ask)
 
-ticks = sim.copy_ticks_from(symbol=symbol, date_from=start.replace(month=12, hour=0, minute=0), count=bars)
-print("is_tester=false\n", ticks)
+print("positions total in MT5: ", sim.positions_total())
 
-"""    
-magic_number = 123456
-slippage = 10
+for pos in sim.positions_get(symbol=symbol):
+    m_trade.position_close(ticket=pos.ticket)
 
-sim.set_magicnumber(magic_number=magic_number) #sets the magic number of a simulator
-sim.set_deviation_in_points(deviation_points=slippage) # sets slippage of the simulator
-
-symbol = symbol
-m_symbol = CSymbolInfo(mt5_instance=mt5)
-m_symbol.name(symbol_name=symbol) # sets the symbol name for the class CSymbolInfo
-
-
-def is_position_exists(type: str) -> bool:
-    
-    for pos in sim.get_positions():
-        if pos["magic"] == magic_number and pos["symbol"] == symbol and pos["type"] == type:
-            return True # position exists
-        
-    return False
-    
-def close_positions(type: str):
-    
-    for pos in sim.get_positions():
-        if pos["magic"] == magic_number and pos["symbol"] == symbol and pos["type"] == type:
-            sim.position_close(pos)
-
-while True:
-    
-    sim.monitor_pending_orders()
-    sim.monitor_positions(verbose=False)
-    sim.monitor_account(verbose=False)
-    
-    sim.run_toolbox_gui()  # Run the simulator toolbox GUI
-    
-    if m_symbol.refresh_rates() is None: # Get recent ticks data from MetaTrader5
-        # print("failed to get recent ticks data")
-        continue
-        
-    if not is_position_exists("buy"): # open a buy trade in a simulator if it doesn't exist
-        sim.buy(volume=0.1, symbol=symbol, open_price=m_symbol.ask())
-    
-    close_positions("buy") # close all buy positions
-    
-    if not is_position_exists("sell"): # open a sell trade in a simulator if it doesn't exist
-        sim.sell(volume=0.1, symbol=symbol, open_price=m_symbol.bid())
-    
-    time.sleep(1) # sleep for one second    
-
-# Market Orders
-
-sim.buy(volume=0.1, symbol=symbol, open_price=m_symbol.ask())
-sim.sell(volume=0.1, symbol=symbol, open_price=m_symbol.bid())
-
-m_trade.buy(volume=0.1, symbol=symbol, price=m_symbol.ask())
-m_trade.sell(volume=0.1, symbol=symbol, price=m_symbol.bid())
-
-
-# Pending Orders
-
-expiry = datetime.now(tz=pytz.UTC) + timedelta(days=1)
-price_gap = 0.0005
-
-# Buy Stop: place above current ask
-sim.buy_stop(volume=0.1, symbol=symbol, open_price=m_symbol.ask() + price_gap, sl=0.0, tp=0.0,
-             comment="Buy Stop Example", expiry_date=expiry, expiration_mode="daily")
-
-m_trade.buy_stop(volume=0.1, symbol=symbol, price=m_symbol.ask() + price_gap)
-
-# Buy Limit: place below current bid
-sim.buy_limit(volume=0.1, symbol=symbol, open_price=m_symbol.bid() - price_gap, sl=0.0, tp=0.0,
-              comment="Buy Limit Example", expiry_date=expiry, expiration_mode="daily_excluding_stops")
-
-m_trade.buy_limit(volume=0.1, symbol=symbol, price=m_symbol.bid() - price_gap)
-
-# Sell Stop: place below current bid
-
-sim.sell_stop(volume=0.1, symbol=symbol, open_price=m_symbol.bid() - price_gap, sl=0.0, tp=0.0,
-              comment="Sell Stop Example", expiry_date=expiry, expiration_mode="gtc")
-
-m_trade.sell_stop(volume=0.1, symbol=symbol, price=m_symbol.ask() - price_gap)
-
-# Sell Limit: place above current ask
-sim.sell_limit(volume=0.1, symbol=symbol, open_price=m_symbol.ask() + price_gap, sl=0.0, tp=0.0,
-               comment="Sell Limit Example", expiry_date=expiry, expiration_mode="gtc")
-
-m_trade.sell_limit(volume=0.1, symbol=symbol, price=m_symbol.bid() + price_gap)
-
-while True: # constantly monitor trades and account metrics
-    
-    sim.monitor_pending_orders()
-    sim.monitor_positions(verbose=False)
-    sim.monitor_account(verbose=False)
-    
-    # sim.run_toolbox_gui()  # Run the simulator toolbox GUI
-    
-    time.sleep(1) # sleep for one second
+print("after close positions total in MT5: ", sim.positions_total())
 """
